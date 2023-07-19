2aa12e03d97e9fdb892d90f99c3cafa9a6eb8209
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-19 11:47:02 -0700

    My 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,11 +14,18 @@ int print_last_digit(int n)
 	if (n < 0)
 		n = -n;
 
+	/* Handle special case for INT_MIN */
+	if (n == 0 || n == 1 << (sizeof(int) * 8 - 1))
+	{
+		_putchar('0' + n % 10);
+		return (n % 10);
+	}
+
 	/* Get the last digit */
 	last_digit = n % 10;
 
 	/* Print the last digit */
-	_putchar(last_digit + '0');
+	_putchar('0' + last_digit);
 
 	/* Return the value of the last digit */
 	return (last_digit);

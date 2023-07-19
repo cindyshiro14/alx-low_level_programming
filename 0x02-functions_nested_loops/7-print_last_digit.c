2a24b88e14df7448d2dcae8820843d1d81987269
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-19 11:43:08 -0700

    My 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number whose last digit needs to be printed
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	/* Get the absolute value of n */
+	if (n < 0)
+		n = -n;
+
+	/* Get the last digit */
+	last_digit = n % 10;
+
+	/* Print the last digit */
+	_putchar(last_digit + '0');
+
+	/* Return the value of the last digit */
+	return (last_digit);
+}

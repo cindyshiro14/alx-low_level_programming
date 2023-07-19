8647e8e9da2f60ec9d15c855a8257154aff3e078
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-19 11:23:37 -0700

    My 4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _islower(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
+int _isalpha(int c);
 
 #endif /* MAIN_H */

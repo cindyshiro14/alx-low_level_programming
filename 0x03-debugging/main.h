fa07e95e8655c881f9afd46e37d2207276d1352d
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 13:31:09 -0700

    My 2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,10 @@
 
 #include <stdio.h>
 
+/* positive_or_negative - Prints if an integer is positive or negative */
 void positive_or_negative(int i);
+
+/* largest_number - Returns the largest of 3 numbers */
 int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */

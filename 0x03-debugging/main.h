fb2d5709dbc3bb760ccdced3e038cc9e53a271d5
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 13:45:11 -0700

    My 3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,18 @@
 
 #include <stdio.h>
 
-/* positive_or_negative - Prints if an integer is positive or negative */
+/* 0. Positive or Negative - Prints if an integer is positive or negative */
 void positive_or_negative(int i);
 
-/* largest_number - Returns the largest of 3 numbers */
+/* 1. Main (Infinite Loop) - Commented out to avoid infinite loop */
+/* int main(void); */
+
+/* 2. Largest Number - Returns the largest of three numbers */
 int largest_number(int a, int b, int c);
+
+/* 3. Leap Year - Converts day of month to day of year and determines
+ * how many days are left in the year, taking leap years into account */
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */

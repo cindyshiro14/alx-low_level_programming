fa2b54345c453c2bc50a4b8357bff16039d1fb77
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 07:05:37 -0700

    My 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _isupper function */
+int _isupper(int c);
+
+/* Function prototype for _putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */

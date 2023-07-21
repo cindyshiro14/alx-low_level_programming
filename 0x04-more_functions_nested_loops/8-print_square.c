727213483b8d63529053f9e162995423fa27dae1
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 08:22:16 -0700

    My 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square of '#' characters in the terminal.
+ * @size: The size of the square to be printed.
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -28,4 +28,7 @@ void print_line(int n);
 /* Function prototype for print_diagonal function */
 void print_diagonal(int n);
 
+/* Function prototype for print_square function */
+void print_square(int size);
+
 #endif /* MAIN_H */

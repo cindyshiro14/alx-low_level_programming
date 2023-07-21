786290899dc4f6ff5b048dd3b1d591eab63c0df2
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 07:26:40 -0700

    My 1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,9 @@
 /* Function prototype for _isupper function */
 int _isupper(int c);
 
+/* Function prototype for _isdigit function */
+int _isdigit(int c);
+
 /* Function prototype for _putchar function */
 int _putchar(char c);
 

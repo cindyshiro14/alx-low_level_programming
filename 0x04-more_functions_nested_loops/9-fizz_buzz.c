a4fd9c1a2234a084c6afbcce0381ff821509545b
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-21 08:40:10 -0700

    My 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * fizz_buzz - Prints numbers from 1 to 100 with Fizz-Buzz replacements
+ * print_fizz_buzz - Prints the numbers from 1 to 100 followed by a new line.
+ *                   For multiples of three print Fizz instead of the number
+ *                   and for the multiples of five print Buzz.
+ *                   For numbers which are multiples of both three and five
+ *                   print FizzBuzz.
+ *
+ * Return: No return value (void).
  */
-void fizz_buzz(void)
+void print_fizz_buzz(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z'), _putchar('B'), _putchar('u'), _putchar('z'), _putchar('z');
+		else if (i % 3 == 0)
+			_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
+		else if (i % 5 == 0)
+			_putchar('B'), _putchar('u'), _putchar('z'), _putchar('z');
+		else
+			print_number(i);
 
-        if (i != 100)
-            printf(" ");
-    }
-    printf("\n");
+		if (i < 100)
+			_putchar(' ');
+		else
+			_putchar('\n');
+	}
 }

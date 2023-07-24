8f658b56e9259d1eb6e279686a5ef6b93c53c8d4
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-24 03:37:28 -0700

    My 1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
+/* Function prototypes */
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _putchar(char c);
 
 #endif /* MAIN_H */

dd6638b81931837070e2cca5ee8f38c03563b2e0
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-25 09:56:12 -0700

    My 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,16 +5,6 @@
 
 /* 0. Function that prints a string */
 void _putchar(char c);
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
 
 /* 1. Function that swaps the values of two integers */
 void swap_int(int *a, int *b);

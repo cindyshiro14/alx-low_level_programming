dd4ca4f53a44fc7d053f4d91febc91def535ddb4
cindyshiro14 <cindyshiro14@gmail.com>
2023-07-31 13:03:11 -0700

    My 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix.
+ * @a: 2D array representing the square matrix.
+ * @size: Size of the square matrix.
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+	int row, col;
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			if (row == col)
+				sum1 += a[row * size + col];
+
+			if (row + col == size - 1)
+				sum2 += a[row * size + col];
+		}
+	}
+
+	_putchar(sum1 / 1000 + '0');
+	_putchar(sum1 / 100 % 10 + '0');
+	_putchar(sum1 / 10 % 10 + '0');
+	_putchar(sum1 % 10 + '0');
+	_putchar(',');
+	_putchar(' ');
+
+	_putchar(sum2 / 1000 + '0');
+	_putchar(sum2 / 100 % 10 + '0');
+	_putchar(sum2 / 10 % 10 + '0');
+	_putchar(sum2 % 10 + '0');
+	_putchar('\n');
+}

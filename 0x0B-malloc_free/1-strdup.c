ebfa9280df22162cc331444ff4b6c740694b002a
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-07 14:10:58 -0700

    My 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,15 +6,16 @@
  * @str: The string to be duplicated
  *
  * Return: On success, returns a pointer to the duplicated string.
- * On failure, returns NULL.
+ *          On failure, returns NULL.
  */
 char *_strdup(char *str)
 {
+char *dup_str;
+unsigned int len = 0, i;
+
 if (str == NULL)
 return (NULL);
 
-char *dup_str;
-unsigned int len = 0;
 while (str[len] != '\0')
 len++;
 
@@ -22,7 +23,6 @@ dup_str = malloc((len + 1) * sizeof(char));
 if (dup_str == NULL)
 return (NULL);
 
-unsigned int i;
 for (i = 0; i <= len; i++)
 dup_str[i] = str[i];
 

6df3d98b272371c29aad822be6b8e9d953dd6a6c
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-07 14:40:09 -0700

    My 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <stdlib.h>
-
 /**
  * count_words - Counts the number of words in a string
  * @str: The input string
@@ -9,62 +6,14 @@
  */
 int count_words(char *str)
 {
-	int count = 0;
-	int i, len = 0;
+int count = 0;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			count++;
-	}
-
-	return (count);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+count++;
 }
 
-/**
- * strtow - Splits a string into words
- * @str: The input string
- *
- * Return: A pointer to an array of strings (words)
- */
-char **strtow(char *str)
-{
-	char **words;
-	int i, j, k, len, word_count = 0;
-
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-
-	word_count = count_words(str);
-
-	words = malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-
-	for (i = 0, k = 0; i < word_count; i++)
-	{
-		while (str[k] == ' ')
-			k++;
-
-		len = 0;
-		while (str[k + len] != ' ' && str[k + len] != '\0')
-			len++;
-
-		words[i] = malloc((len + 1) * sizeof(char));
-		if (words[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-				free(words[j]);
-			free(words);
-			return (NULL);
-		}
-
-		for (j = 0; j < len; j++)
-			words[i][j] = str[k++];
-
-		words[i][j] = '\0';
-	}
-
-	words[i] = NULL;
-	return (words);
+return (count);
 }

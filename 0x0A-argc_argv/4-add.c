5151620e29eecd28a2850ae4892619904a99bcc5
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-09 16:39:43 -0700

    My 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,54 +1,34 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdlib.h> /* for atoi */
 
 /**
- * is_positive_number - Check if a string is a positive number.
- * @str: The input string to check.
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the arguments
  *
- * Return: true if the string is a positive number, false otherwise.
- */
-bool is_positive_number(const char *str)
-{
-	if (*str == '\0')
-		return (false);
-
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-			return (false);
-		str++;
-	}
-
-	return (true);
-}
-
-/**
- * main - Entry point of the program for the Add task.
- * @argc: The number of command-line arguments.
- * @argv: An array of pointers to the command-line arguments.
- *
- * Return: Always 0.
+ * Return: 0 if successful, 1 if an error occurred
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+int i, sum = 0;
 
-	for (int i = 1; i < argc; i++)
-	{
-		if (is_positive_number(argv[i]))
-		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
+for (i = 1; i < argc; i++)
+{
+int j;
 
-	printf("%d\n", sum);
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
 
-	return (0);
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+
+return (0);
 }

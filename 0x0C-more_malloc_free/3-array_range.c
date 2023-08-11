77b50003182690c731f3ced8427b936bed8a35a3
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-11 14:24:15 -0700

    My 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,22 +12,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
-char *char_ptr;
-unsigned int i;
-
 if (nmemb == 0 || size == 0)
-return (NULL);
+return NULL;
 
-ptr = malloc(nmemb * size);
+unsigned int total_size = nmemb * size;
+void *ptr = malloc(total_size);
 
 if (ptr == NULL)
-return (NULL);
+return NULL;
 
-char_ptr = ptr;
+unsigned char *byte_ptr = ptr;
+for (unsigned int i = 0; i < total_size; i++)
+byte_ptr[i] = 0;
 
-for (i = 0; i < nmemb * size; i++)
-char_ptr[i] = 0;
-
-return (ptr);
+return ptr;
 }

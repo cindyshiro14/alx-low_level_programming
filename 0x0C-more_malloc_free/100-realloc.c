3a61f54cc2485212463652a9d8f0fa8467ea1055
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-11 15:03:09 -0700

    My 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,43 +2,44 @@
 #include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr: Pointer to the memory previously allocated
- * @old_size: Size of the allocated space for ptr
- * @new_size: New size of the memory block
+ * _realloc - Reallocate memory block using malloc and free
+ * @ptr: Pointer to the memory block to reallocate
+ * @old_size: Size of the old memory block
+ * @new_size: Size of the new memory block
  *
- * Return: Pointer to the reallocated memory block
+ * Return: Pointer to the newly reallocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+    void *new_ptr;
+
+    if (new_size == old_size)
+        return (ptr);
+
+    if (ptr == NULL)
+        return (malloc(new_size));
+
     if (new_size == 0)
     {
         free(ptr);
         return (NULL);
     }
 
-    if (!ptr)
-    {
-        return (malloc(new_size));
-    }
-
-    if (new_size <= old_size)
-    {
-        return (ptr);
-    }
-
-    void *new_ptr = malloc(new_size);
-    if (!new_ptr)
-    {
-        free(ptr);
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
         return (NULL);
-    }
 
-    for (unsigned int i = 0; i < old_size; i++)
+    /* Copy contents from old block to new block */
+    char *old_ptr = ptr;
+    char *new_ptr_char = new_ptr;
+    unsigned int min_size = (old_size < new_size) ? old_size : new_size;
+    for (unsigned int i = 0; i < min_size; i++)
     {
-        ((char *)new_ptr)[i] = ((char *)ptr)[i];
+        new_ptr_char[i] = old_ptr[i];
     }
 
+    /* Free the old block */
     free(ptr);
+
     return (new_ptr);
 }

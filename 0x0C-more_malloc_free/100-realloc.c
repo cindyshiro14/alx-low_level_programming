dd44ab203b610a8b4c7f3ca054a95ade1f006ec2
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-11 14:47:36 -0700

    My 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *new_ptr;
+
+    if (ptr == NULL)
+        return (malloc(new_size));
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    if (new_size <= old_size)
+        return (ptr);
+
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+        return (NULL);
+
+    for (unsigned int i = 0; i < old_size; i++)
+        *((char *)new_ptr + i) = *((char *)ptr + i);
+
+    free(ptr);
+    return (new_ptr);
+}

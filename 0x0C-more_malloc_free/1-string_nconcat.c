e102b6628ec6aaad928eff81642946f1a3ca601b
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-11 14:04:13 -0700

    My 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,28 +9,35 @@
  * @n: Number of bytes to concatenate from s2
  *
  * Return: Pointer to the newly allocated concatenated string
- *         If memory allocation fails, returns NULL
+ *         If memory allocation fails or s1/s2 is NULL, returns NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+    unsigned int len1 = 0;
+    unsigned int len2 = 0;
+    char *concatenated;
+    unsigned int i;
 
-    unsigned int len1 = strlen(s1);
-    unsigned int len2 = strlen(s2);
+    if (s1 != NULL)
+        len1 = strlen(s1);
+    if (s2 != NULL)
+        len2 = strlen(s2);
 
     if (n >= len2)
         n = len2;
 
-    char *result = malloc(len1 + n + 1);
+    concatenated = malloc(len1 + n + 1);
 
-    if (result == NULL)
+    if (concatenated == NULL)
         return (NULL);
 
-    strcpy(result, s1);
-    strncat(result, s2, n);
+    for (i = 0; i < len1; i++)
+        concatenated[i] = s1[i];
 
-    return (result);
+    for (i = 0; i < n; i++)
+        concatenated[len1 + i] = s2[i];
+
+    concatenated[len1 + n] = '\0';
+
+    return (concatenated);
 }

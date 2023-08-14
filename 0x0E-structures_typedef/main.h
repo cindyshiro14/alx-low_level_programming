c4ff247c28ecc86aa70de9e1114bab6cffa345e6
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-14 14:22:27 -0700

    My 2-print_dog.c

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure defines the attributes of a dog.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H */

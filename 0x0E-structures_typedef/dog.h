35885fbfcff29825e06481d668aeadcdbc214242
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-15 12:45:25 -0700

    My 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Defines a dog's attributes.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: This structure defines the attributes of a dog,
- * including its name, age, and owner.
- */
 struct dog
 {
-    char name[100];
+    char *name;
     float age;
-    char owner[100];
+    char *owner;
 };
-
-/* Prototype for the init_dog function */
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */

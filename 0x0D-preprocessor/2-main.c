ac867e4b7d106a0622c96806e569e7e7bb6c216e
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-15 14:18:22 -0700

    My 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the current source file.
+ *
+ * Description: This program demonstrates the usage of the __FILE__ predefined
+ * macro to print the name of the current source file. The program outputs the
+ * name of the file it was compiled from.
+ *
+ * Return: Always 0 (indicating success).
+ */
 int main(void)
 {
 printf("%s\n", __FILE__);

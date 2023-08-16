817dca963024c8c094999b099f7d770680cabda0
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-16 10:29:37 -0700

    My 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,53 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name using the provided function.
- * @name: Pointer to the name to be printed.
- * @f: Pointer to a function that prints the name.
+ * print_name - prints a name using a specified printing function
+ * @name: name to print
+ * @f: pointer to the printing function to use
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
-}
-
-/**
- * print_name_as_is - Prints a name as it is.
- * @name: Pointer to the name to be printed.
- */
-void print_name_as_is(char *name)
-{
-	while (*name)
-	{
-		putchar(*name);
-		name++;
-	}
-	putchar('\n');
-}
-
-/**
- * print_name_uppercase - Prints a name in uppercase.
- * @name: Pointer to the name to be printed.
- */
-void print_name_uppercase(char *name)
-{
-	while (*name)
-	{
-		if (*name >= 'a' && *name <= 'z')
-			putchar(*name + 'A' - 'a');
-		else
-			putchar(*name);
-		name++;
-	}
-	putchar('\n');
-}
-
-/**
- * main - Entry point of the program.
- * Return: Always 0.
- */
-int main(void)
-{
-	print_name("Bob", print_name_as_is);
-	print_name("Bob Dylan", print_name_uppercase);
-	return (0);
+if (name != NULL && f != NULL)
+f(name);
 }

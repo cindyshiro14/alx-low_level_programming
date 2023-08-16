b67605f2f3af2f0ef178e1862046028f6885f245
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-16 10:20:36 -0700

    My 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-/* Calls a function to print a name */
-void print_name(char *name, void (*f)(char *));
-
-/* Prints a name as it is */
-void print_name_as_is(char *name)
-{
-printf("Hello, my name is %s\n", name);
-}
-
-/* Prints a name in uppercase */
-void print_name_uppercase(char *name)
-{
-unsigned int i;
-printf("Hello, my uppercase name is ");
-i = 0;
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-{
-putchar(name[i] + 'A' - 'a');
-}
-else
-{
-putchar(name[i]);
-}
-i++;
-}
-}
-
-/* Entry point of the program */
-int main(void)
-{
-print_name("Bob", print_name_as_is);
-print_name("Bob Dylan", print_name_uppercase);
-return (0);
-}
-
-/* Calls the provided function to print the name */
+/**
+ * print_name - Prints a name using the provided function.
+ * @name: Pointer to the name to be printed.
+ * @f: Pointer to a function that prints the name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+	f(name);
+}
+
+/**
+ * print_name_as_is - Prints a name as it is.
+ * @name: Pointer to the name to be printed.
+ */
+void print_name_as_is(char *name)
+{
+	while (*name)
+	{
+		putchar(*name);
+		name++;
+	}
+	putchar('\n');
+}
+
+/**
+ * print_name_uppercase - Prints a name in uppercase.
+ * @name: Pointer to the name to be printed.
+ */
+void print_name_uppercase(char *name)
+{
+	while (*name)
+	{
+		if (*name >= 'a' && *name <= 'z')
+			putchar(*name + 'A' - 'a');
+		else
+			putchar(*name);
+		name++;
+	}
+	putchar('\n');
+}
+
+/**
+ * main - Entry point of the program.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_name("Bob", print_name_as_is);
+	print_name("Bob Dylan", print_name_uppercase);
+	return (0);
 }

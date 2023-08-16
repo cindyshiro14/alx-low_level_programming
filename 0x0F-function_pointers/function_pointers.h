93f1fef369227ede69f85f6e7c2c26876ad93aef
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-16 10:14:08 -0700

    My 0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdio.h>
+#include <stddef.h> /*standard library header for null */
+
+/*function prototype for the  _putchar function*/
+int _putchar(char c);
 
 /* Function prototype for the print_name function */
 void print_name(char *name, void (*f)(char *));

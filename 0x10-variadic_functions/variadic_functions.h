1f06793374e8e20d4bb89913125f1bb4d872530e
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-17 04:57:51 -0700

    My 1-print_numbers.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
-int _putchar(char c); /* Include _putchar prototype */
+void print_numbers(const char *separator, const unsigned int n, ...); // Prototype for print_numbers
+int _putchar(char c);
 
 #endif /* VARIADIC_FUNCTIONS_H */

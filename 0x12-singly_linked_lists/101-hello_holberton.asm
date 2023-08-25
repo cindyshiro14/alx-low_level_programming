832711be055531a7c96e4bada668e6a60ee6e7d2
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-25 05:32:18 -0700

    My 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db "Hello, Holberton", 0   ; Null-terminated string
+    format db "%s", 10, 0            ; Format string for printf
 
 section .text
     global main
@@ -7,9 +8,10 @@ section .text
 
 main:
     push rbp             ; Save base pointer
-    mov rdi, hello       ; Load address of the string into rdi
+    mov rdi, format      ; Load format string into rdi
+    mov rsi, hello       ; Load address of the string into rsi
+    xor rax, rax         ; Clear rax register before syscall
     call printf         ; Call printf function
-    add rsp, 8           ; Adjust stack after the call
     pop rbp              ; Restore base pointer
 
     xor rax, rax         ; Return 0

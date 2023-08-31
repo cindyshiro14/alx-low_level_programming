f9e94d8750dee81f3d06060ca6d9735ef692fb97
cindyshiro14 <cindyshiro14@gmail.com>
2023-08-31 12:40:25 -0700

    My 1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h> // For NULL definition
+#include <unistd.h> // For the write function
+
+/* Function prototype for printing characters */
+int _putchar(char c);
+
+/* Function prototype for converting binary to unsigned int */
 unsigned int binary_to_uint(const char *b);
+
+/* Function prototype for printing the binary representation of a number */
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */

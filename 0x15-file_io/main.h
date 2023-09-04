e31e7fad26e7962f892ce2ac8f50fd1d94ba5e2d
cindyshiro14 <cindyshiro14@gmail.com>
2023-09-04 15:01:25 -0700

    My 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int exit_code, const char *message);
 int putchar(int c);
 
 #endif /* MAIN_H */

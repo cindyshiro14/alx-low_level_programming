72040fb296498d68075836a1953182d8a85ea1bd
cindyshiro14 <cindyshiro14@gmail.com>
2023-09-04 14:35:30 -0700

    My 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 int create_file(const char *filename, char *text_content)
 {
-if (filename == NULL)
-return (-1);
+    if (filename == NULL)
+        return (-1);
 
-int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); /* rw------- */
+    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); // rw-------
 
-if (fd == -1)
-return (-1);
+    if (fd == -1)
+        return (-1);
 
-if (text_content != NULL)
-{
-ssize_t write_result = write(fd, text_content, strlen(text_content));
-close(fd);
+    ssize_t write_result = -1;
 
-if (write_result == -1)
-return (-1);
-}
-else
-{
-close(fd);
-}
+    if (text_content != NULL)
+    {
+        write_result = write(fd, text_content, strlen(text_content));
+    }
 
-return (1);
+    close(fd);
+
+    if (write_result == -1)
+        return (-1);
+
+    return (1);
 }

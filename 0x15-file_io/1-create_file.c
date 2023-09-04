2391d26af13a05c3a80ef735cfec928dc405489a
cindyshiro14 <cindyshiro14@gmail.com>
2023-09-04 14:45:33 -0700

    My 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,25 +7,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return (-1);
+int fd;
+ssize_t write_result;
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); // rw-------
+if (filename == NULL)
+return (-1);
 
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    ssize_t write_result = -1;
+if (fd == -1)
+return (-1);
 
-    if (text_content != NULL)
-    {
-        write_result = write(fd, text_content, strlen(text_content));
-    }
+write_result = -1;
 
-    close(fd);
+if (text_content != NULL)
+{
+write_result = write(fd, text_content, strlen(text_content));
+}
 
-    if (write_result == -1)
-        return (-1);
+close(fd);
 
-    return (1);
+if (write_result == -1)
+return (-1);
+
+return (1);
 }

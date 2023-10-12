94cfbc83fcebb5474690889c10313c698e8fc683
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-12 04:28:02 -0700

    My 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,38 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
+
+/* 0-print_dlistint.c */
+size_t print_dlistint(const dlistint_t *h);
+
+/* 1-dlistint_len.c */
+size_t dlistint_len(const dlistint_t *h);
+
+/* 2-add_dnodeint.c */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+/* 3-add_dnodeint_end.c */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+/* 4-free_dlistint.c */
+void free_dlistint(dlistint_t *head);
+
+#endif /* LISTS_H */

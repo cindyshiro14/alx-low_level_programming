0cfdf87f6c7200a46f8e35fc5b10b1a0e307ce41
cindyshiro14 <cindyshiro14@gmail.com>
2023-11-03 09:58:36 -0700

    My 1-djb2.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,15 +7,15 @@
 /* Structure declarations */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 /* Function prototypes */
